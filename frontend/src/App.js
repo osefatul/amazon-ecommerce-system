1140d71bc1e475e2597731518823fa0717b00ec6
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-10-20 01:59:47 -0700

    added it to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      Learn React
     </div>
   );
 }
